dfcc4ec0c01da036f87e49bb0a3077e9a5da24c1
Ashley Arrue <ashleydarrue@gmail.com>
2019-09-12 23:07:18 -0400

    ui change

--- views/times.js
+++ views/times.js
@@ -1,3 +1,6 @@
-$(document).ready(function(){
-  $('.parallax').parallax();
+window.onload = function(e){ 
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.parallax');
+  var instances = M.Parallax.init(elems, options);
 });
+}
